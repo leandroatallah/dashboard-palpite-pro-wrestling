80cbbe8a2ba8ffad98c13ccb98de23ec0269fd97
Leandro Atallah <leandroatallah@gmail.com>
2022-12-11 18:10:09 -0300

    Clear session when token not found

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
+import {queryClient} from './query';
 
 const API_URL =
   process.env.NODE_ENV !== 'development'
     ? import.meta.env.VITE_API_PRODUCTION_URL
     : import.meta.env.VITE_API_LOCAL_URL;
+
+function clearAllSession() {
+  localStorage.removeItem('token');
+  queryClient.removeQueries();
+}
 
 axios.defaults.baseURL = API_URL;
 axios.defaults.timeout = 99999;
@@ -14,7 +20,8 @@ axios.defaults.headers = {
 
 axios.interceptors.request.use(
   async (config) => {
-    if (config.url === '/user/login') {
+    const isLoginRoute = config.url === '/user/login';
+    if (isLoginRoute) {
       config.headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
       };
@@ -27,7 +34,13 @@ axios.interceptors.request.use(
         accept: 'application/json',
         authorization: `Bearer ${JSON.parse(token)}`,
       };
+    } else {
+      if (!isLoginRoute) {
+        clearAllSession();
+        return (window.location = '/login');
+      }
     }
+
     return config;
   },
   function (error) {
@@ -38,6 +51,13 @@ axios.interceptors.request.use(
 axios.interceptors.response.use(
   (response) => response,
   (error) => {
+    const status = error.response?.status;
+
+    if (status && (status === 401 || status === 403)) {
+      clearAllSession();
+      return (window.location = '/login');
+    }
+
     return Promise.reject(error);
   }
 );
